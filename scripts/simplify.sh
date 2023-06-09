84e353137b0bd573419dede68c800d734f034344
ling <1042585959@qq.com>
2023-06-09 09:26:55 +0800

    myconfig

--- scripts/simplify.sh
+++ scripts/simplify.sh
@@ -85,7 +85,7 @@ svn export https://github.com/Siriling/5G-Modem-Support/trunk/quectel_MHI packag
 
 # 添加5G模组拨号脚本
 mkdir package/base-files/files/root
-cp -f $GITHUB_WORKSPACE/tools/5G模组拨号脚本/5GModem/* package/base-files/files/root/5GModem
+cp -rf $GITHUB_WORKSPACE/tools/5G模组拨号脚本/* package/base-files/files/root
 chmod -R 755 package/base-files/files/root/5GModem
 cp -rf $GITHUB_WORKSPACE/configs/etc/* package/base-files/files/etc
 
