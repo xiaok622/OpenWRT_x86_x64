c5382c4e58270b9b4b2034b4683a8931a1ca79f0
ling <1042585959@qq.com>
2023-10-06 13:15:59 +0800

    更新配置

--- scripts/full.sh
+++ scripts/full.sh
@@ -33,11 +33,13 @@ svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-goweb
 
 # 科学上网和代理应用
 #SSR
-svn export https://github.com/fw876/helloworld/trunk helloworld
-#Passwall和Passwall2
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk openwrt-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
+# svn export https://github.com/fw876/helloworld/trunk helloworld
+#SSR+Passwall和Passwall2
+# svn export https://github.com/xiaorouji/openwrt-passwall/trunk openwrt-passwall
+# svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+# svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
+svn export https://github.com/kenzok8/small/trunk ssrAndPasswall
+
 #VSSR（Hello Word）
 svn export https://github.com/jerrykuku/lua-maxminddb/trunk lua-maxminddb
 svn export https://github.com/jerrykuku/luci-app-vssr/trunk luci-app-vssr
