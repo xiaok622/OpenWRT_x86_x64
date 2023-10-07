ee7699283e68087b8aef4b80b8e736d784c9247c
ling <1042585959@qq.com>
2023-10-07 13:32:02 +0800

    更新配置

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -39,9 +39,3 @@ echo "src-link luci $luci_feed" >> feeds.conf.default
 
 # Update feeds
 ./scripts/feeds update -a
-
-# 编译新版Sing-box和hysteria，需golang版本1.20或者以上版本
-pushd feeds/packages/lang
-rm -rf golang
-svn co https://github.com/openwrt/packages/branches/openwrt-23.05/lang/golang
-popd
